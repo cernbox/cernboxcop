cc127fca3563c799bec7ffdcfc42f6360b46ad78
Gianmaria Del Monte <g.macmount@gmail.com>
2021-03-23 11:14:35 +0100

    fix bug print

--- cmd/spin_status.go
+++ cmd/spin_status.go
@@ -53,8 +53,12 @@ func (s *SpinStatus) getDescription() string {
 	}
 }
 
-func (s *SpinStatus) print() {
-	fmt.Fprintf(os.Stderr, "\r %s %s %s", s.spin.Current(), s.info, s.getDescription())
+func (s *SpinStatus) printStatus(status string) {
+	fmt.Fprintf(os.Stderr, "\033[2K\r %s %s %s", status, s.info, s.getDescription())
+}
+
+func (s *SpinStatus) refresh() {
+	s.printStatus(s.spin.Current())
 }
 
 func (s *SpinStatus) Start() {
@@ -63,28 +67,28 @@ func (s *SpinStatus) Start() {
 		for range s.ticker.C {
 			s.spin.Next()
 			s.mu.Lock()
-			s.print()
+			s.refresh()
 			s.mu.Unlock()
 		}
 	}()
-	s.print()
+	s.refresh()
 }
 
 func (s *SpinStatus) Done() {
 	s.ticker.Stop()
-	fmt.Fprintf(os.Stderr, "\r \033[32m✓\033[0m %s %s\n", s.info, s.getDescription())
+	s.printStatus("\033[32m✓\033[0m")
 }
 
 func (s *SpinStatus) Update(delta int) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 	s.current += delta
-	s.print()
+	s.refresh()
 }
 
 func (s *SpinStatus) UpdateDescription(newDesc string) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 	s.description = newDesc
-	s.print()
+	s.refresh()
 }
